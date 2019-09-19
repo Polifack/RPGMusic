a5962719f69681f610f2aef2f6c2cde57a80f4a1
poli <diegoroca.ordes@gmail.com>
2019-09-19 14:26:50 +0000

    Multiple playing is now allowed

--- src/handler.py
+++ src/handler.py
@@ -5,32 +5,51 @@ import threading
 import time
 from tkinter import *
 from tkinter.filedialog import askopenfilename
+from .view import TrackPlayer
+
+class PlayerHandler:
+    #Handler para cada uno de los reproductores de música. Estes se ejecutan en un Thread separado.
+    #Necesita una vista propia, un modelo compartido entre handlers, un archivo (o lista de archivos) y un tinkerer
+    def __init__ (self, view, model, file, tinkerer):
+        print("* Creating new player handler...")
+        self.view = view(file)
+        self.model = model
+        self.view.connect(self.on_play_clicked, self.on_stop_clicked, self.on_files_clicked)
+        self.file = file
+    
+    #Función que actualiza la barra de progreso de reproducción. Se ejecuta en un thread aparte.
+    #Utiliza los hash de archivos del modelo para comprobar el progreso y si se está ejecutando o no
+    def update_progress(self):
+        while (not self.model.runhash[self.file]):
+            time.sleep(0.05)
+            self.view.update_prog_bar(self.model.proghash[self.file])
+        self.view.change_state()
+    
+    #Callback del boton 'PLAY'
+    def on_play_clicked(self, button):
+        self.model.play(self.file)
+        self.view.change_state()
+        threading.Thread(target = self.update_progress, args =(), daemon = True).start()
+    
+    #Callback del boton 'STOP'
+    def on_stop_clicked(self, button):
+        self.model.stop(self.file)
+    
+    #Callback del boton 'ARCHIVOS'
+    def on_files_clicked(self, button):
+        self.file = askopenfilename()
 
 class Handler:
-    #Constructor. Inicializamos la vista y el modelo.
+    #Handler principal de la aplicación
     def __init__(self, v, m):
         self.view = v()
         self.model = m()
         self.view.connect(self)
         self.tk = Tk().withdraw()
         self.file = None
-    
-    def progress_playing(self, id):
-        while(not self.model.runhash[id]):            
-            time.sleep(0.1)
-            self.view.player.update_prog_bar(self.model.proghash[id])
-        self.view.player.change_state()
-        
 
-    #Nota: Todas los metodos de una clase en python llevan como primer argumento a si misma
-    def on_play_clicked(self, button):
-        self.model.play(self.file)
-        self.view.player.change_state()
-        threading.Thread(target = self.progress_playing, args =(self.file,), daemon = True).start()
-    
-    def on_stop_clicked(self, button):
-        self.model.stop(self.file)
-    
-    def on_files_clicked(self, button):
-        self.file = askopenfilename()
-        self.view.enable_button()
+    #Callback de añadir reproductor. Crea un nuevo reproductor y actualiza la vista.
+    def on_add_player_clicked(self, button):
+        track = askopenfilename()
+        player_handler = PlayerHandler(TrackPlayer, self.model, track, self.tk)
+        self.view.add_music_player(player_handler.view.track_player, player_handler.file)
--- src/view.py
+++ src/view.py
@@ -1,21 +1,29 @@
 from gi.repository import Gtk, GLib
 
 class TrackPlayer:
-    def __init__(self):
+    def __init__(self, filename):
+        print("* Creating new player view...")
+
         #State logic
         self.state = 'PLAY'
+
+        #Select file button
+        self.file_button = Gtk.Button(label = "FILE")        
 
         #Play/Pause button
         self.play_button = Gtk.Button(label = self.state)
         self.play_button.connect('clicked', self.execute_function)
-        self.play_button.set_sensitive(False)
         
         #Progress bar
         self.play_progress = Gtk.ProgressBar()
 
+        #File Name Label
+        self.label = Gtk.Label(filename)
+
         #Layout
         box = Gtk.Box(spacing=6, orientation=Gtk.Orientation.HORIZONTAL)
-        box.pack_start(self.play_button, False, False, 0) #child, expand, fill, padding
+        box.pack_start(self.label, False, False, 0)
+        box.pack_start(self.play_button, False, False, 0)
         nowplaybox = Gtk.Box(spacing=6, orientation = Gtk.Orientation.VERTICAL)
         nowplaybox.pack_start(self.play_progress, False, False, 6)
         box.pack_start(nowplaybox, True, True, 6)
@@ -26,9 +34,10 @@ class TrackPlayer:
     def update_prog_bar(self, value):
         self.play_progress.set_fraction(value)
 
-    def connect(self, play_click, stop_click):
+    def connect(self, play_click, stop_click, file_click):
         self.play_click = play_click
         self.stop_click = stop_click
+        self.file_button.connect('clicked', file_click)
 
     def execute_function(self, button):
         if (self.state == 'STOP'):
@@ -48,22 +57,23 @@ class TrackPlayer:
 class View:
     def __init__(self):
         ##TRACK PLAYER
-        self.player = TrackPlayer()
-        self.file_button = Gtk.Button(label = "Open File Manager")
+        self.add_button = Gtk.Button(label = "[+]")
+        self.music_players = dict()
 
         self.main_window = Gtk.Box(spacing=6,orientation=Gtk.Orientation.VERTICAL)
-        self.main_window.pack_start(self.file_button, False, False, 0)
-        self.main_window.pack_start(self.player.track_player, False, False, 0)
+        self.main_window.pack_start(self.add_button, False, False, 0)
 
         self.win = Gtk.Window(title="RolPlayer")
+        self.win.set_default_size(1024, 720)
         self.win.add(self.main_window)
         self.win.connect("destroy", Gtk.main_quit)
         self.win.show_all()
 
     def connect(self, handler):
-        self.player.connect(handler.on_play_clicked, handler.on_stop_clicked)
-        self.file_button.connect('clicked',handler.on_files_clicked)
+        self.add_button.connect('clicked',handler.on_add_player_clicked)
     
-    def enable_button(self):
-        print ("enabling button...")
-        self.player.play_button.set_sensitive(True)
+    #Adds a new player
+    def add_music_player(self, player, file):
+        self.music_players[file] = player
+        self.main_window.pack_start(player, False, False, 0)
+        player.show_all()
